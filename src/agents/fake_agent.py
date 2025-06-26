492a446af18eb7ba70efac3f1eed41df4274b58e
romj99 <99736241+romj99@users.noreply.github.com>
2025-06-26 14:19:41 +0200

    base agent class, LLMAgent, FakeAgent

---
+++ src/agents/fake_agent.py
@@ -0,0 +1,22 @@
+import numpy as np
+from src.agents.base_agent import Agent
+
+class FakeAgent(Agent):
+    def __init__(self, name: str, time_series_data: np.ndarray, nbr_rounds:int, **kwargs):
+        super().__init__(name=name, **kwargs)
+        assert len(time_series_data) >= nbr_rounds, "Time series can't be smaller than the number of rounds"
+        self.time_series_data = time_series_data
+        self.current_index = 0
+
+    async def act(self, prompt: str) -> dict:
+        chosen_price = self.time_series_data[self.current_index]
+        self.current_index += 1
+        return {'agent_name': self.name, 'content': {'chosen_price': float(chosen_price)}}
+    
+    @property
+    def requires_prompt(self) -> bool:
+        return False
+    
+    @property
+    def type(self) -> bool:
+        return "fake_agent"
